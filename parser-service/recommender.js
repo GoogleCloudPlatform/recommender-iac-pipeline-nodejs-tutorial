061c87ec71293e2a2c77ca4c6ffaf50b0d687de9
Garron Haun <thefsm93@gmail.com>
2023-09-28 09:48:30 -0700

    Pushing final changes

--- parser-service/recommender.js
+++ parser-service/recommender.js
@@ -88,8 +88,25 @@ const setRecommendationStatus = async (recommendationsIDsAndETags, newStatus) =>
         Authorization: `Bearer ${accessToken.token}`,
         'x-goog-user-project': id.split('/')[1],
       },
-    }
-  ));
+    }).catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+        console.log(error.config);
+    })
+  );
 
   await Promise.all(promises);
 };
